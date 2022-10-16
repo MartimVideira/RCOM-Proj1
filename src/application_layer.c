02a670f0e704f5f862727734a11fc9704abe7825
Martim <mitram790@gmail.com>
2022-10-16 22:29:22 +0100

    Created Packet types and subtypes to build application packets

--- src/application_layer.c
+++ src/application_layer.c
@@ -9,10 +9,54 @@
 #include <libgen.h>
 #include <string.h>
 
-void applicationLayer(const char *serialPort, const char *role, int baudRate,
-                      int nTries, int timeout, const char *filename)
-{
-    // TODO
+#include "../include/link_layer.h"
+
+typedef unsigned char byte;
+
+
+typedef struct{
+    byte sequenceNumber;
+    byte dataSize[2];
+    byte* data;
+
+} DataPacket;
+
+enum ParameterType{
+    PT_FILE_SIZE = 1,
+    PT_FILE_NAME = 2
+};
+
+typedef struct {
+    enum ParameterType type;
+    byte length;
+    byte* value;
+
+} Tlv;
+
+typedef struct{ 
+    Tlv* buf;
+    int size;
+} TlvArray;
+
+enum PacketControl{
+    PC_DATA = 1,
+    PC_START = 2,
+    PC_END = 3
+};
+
+typedef struct{
+
+    enum PacketControl control;
+    union {
+        TlvArray tlvArray;
+        DataPacket data;
+    } content;
+
+} Packet;
+
+
+void applicationSend(LinkLayer parameters,const char *filename){
+
     struct stat fileStat;
     if (stat(filename,&fileStat) != 0){
         printf("File does not exist\n");
@@ -32,8 +76,13 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     // Já consigo ver se o ficheiro existe e não é um diretorio tenho acesso ao nome dele
     // Falta secalhar truncar e considerar so o nome do ficheiro e não o path ver como isso será possivel
     // Depois falta abrir em modo binário e comecar a ler o ficheiro!
-    // Depois de ver que consigo ler o ficheiro definir o pacote de dados
-    // Usar a interface com a link layer 
-    // Criar um LinkLayer paraameters
-    
+}
+void applicationRecieve(LinkLayer parameters){
+    // What if that fileName already exists?
+}
+
+void applicationLayer(const char *serialPort, const char *role, int baudRate,
+                      int nTries, int timeout, const char *filename)
+{
+    // Create Linklayer parameters and do a switch on the role
 }
